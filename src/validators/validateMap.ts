27e9fa3f45ef2a4621c6e12c8a21ff6ccdcab23f
Dejan Sandic <dejan.s@aleacontrol.com>
2019-11-11 13:43:25 +0100

    Add validateMap function

---
+++ src/validators/validateMap.ts
@@ -0,0 +1,102 @@
+import validateValue from './validateValue';
+import validateObject from './validateObject';
+import { combinePropNames, isTypeConstructor, getType } from './helpers';
+
+// Only way for jest to track recursive calls is for the
+// validateMap to be imported into its own scope
+import * as self from './validateMap';
+
+interface MapOptions {
+	required?: boolean;
+	validate?: (prop: string, val: any) => void;
+}
+
+interface Blueprint {
+	[key: string]: any;
+}
+
+/**
+ * Function for validating SchemaMaps
+ * SchemaMap: (object which has properties with defined types)
+ * 
+ * @param { string | null } parentProp 
+ * @param { Object } schema blueprint for parentProp
+ * @param { object } options options object
+ * @param { Object } data 
+ */
+export default function validateMap(parentProp: string, schema: Blueprint, options: MapOptions, data: any) {
+	const { required = true, validate } = options;
+
+	if (typeof data === 'undefined') {
+		if (required)
+			throw new Error(`Property ${parentProp} is defined on the schema but it is missing on the provided data.`);
+		else return true;
+	}
+
+	const schemaKeys = Object.keys(schema);
+	const dataKeys = Object.keys(data);
+
+	// Check does the provided object have properties not defined on the schema
+	dataKeys.forEach((key) => {
+		if (!schemaKeys.includes(key)) {
+			const prop = combinePropNames(parentProp, key);
+			throw new Error(`Property ${prop} on the provided data is not defined on the schema.`);
+		}
+	});
+
+	// Validate values
+	schemaKeys.forEach((key) => {
+		const prop = combinePropNames(parentProp, key);
+		const validator = schema[key];
+		const value = data[key];
+
+		/**
+		 * If validator is constructor (String, Object, etc.)
+		 * pass the prop, validator, value and empty options object
+		 * to the validateValue() function
+		 */
+		if (isTypeConstructor(validator)) {
+			return validateValue(prop, validator, {}, value);
+		}
+
+		/**
+		 * If validator is an object which has type property which is
+		 * the constructor, destructure constructor and options object
+		 * from it and pass them to the validateValue() function
+		 */
+		if (isTypeConstructor(validator.type)) {
+			const { type, ...options } = validator;
+			return validateValue(prop, type, options, value);
+		}
+
+		// If validator is an array pass it to the validateList() function
+		// if (Array.isArray(validator)) {
+		// 	return validateList(prop, validator, {}, value);
+		// }
+
+		/**
+		 * If validator is an object, first check is the value object as well
+		 * and if it is pass it to the validateMap() function
+		 */
+		if (typeof validator === 'object') {
+			validateObject(prop, {}, value);
+			return self.default(prop, validator, {}, value);
+		}
+
+		// Throw an error if validator is not constructor, object or array
+		throw new Error(
+			`Unsuported schema type for the property '${prop}' (${getType(validator)}).` +
+				` Check the documentation for the supported schema types.`
+		);
+	});
+
+	if (validate) {
+		if (typeof validate === 'function') validate(parentProp, data);
+		else
+			throw new Error(
+				`Validator for the property ${parentProp} should be a function, ${getType(validate)} found instead.`
+			);
+	}
+
+	return true;
+}
---
+++ test/validateMap.test.ts
@@ -0,0 +1,108 @@
+import validateMap from '../src/validators/validateMap';
+import * as validateMapExport from '../src/validators/validateMap';
+
+// Import inner functions used by the validateMap()
+import validateValue from '../src/validators/validateValue';
+import validateObject from '../src/validators/validateObject';
+
+// Mock inner functions used by the validateMap()
+jest.mock('../src/validators/validateValue');
+jest.mock('../src/validators/validateObject');
+
+const options = {};
+
+test('validateMap should throw an error if the property defined on the blueprint is not provided.', () => {
+	const fnUndf = () => validateMap('testProp', {}, options, undefined);
+	expect(fnUndf).toThrowError('Property testProp is defined on the schema but it is missing on the provided data.');
+});
+
+test(
+	'validateMap should not throw an error if the property defined on the' +
+		'blueprint is not provided, but it is not required.',
+	() => {
+		const fnUndf = () => validateMap('testProp', {}, { required: false }, undefined);
+		expect(fnUndf).not.toThrow();
+	}
+);
+
+test('validateMap should throw an error if the provided property is not defined on the blueprint.', () => {
+	const validateFn = () => validateMap('testProp', {}, options, { a: 1 });
+	expect(validateFn).toThrowError('Property testProp.a on the provided data is not defined on the schema.');
+});
+
+test(
+	'validateMap should invoke validateValue if validator is the type constructor\n' +
+		'or validator has the type property wich is the type constructor.',
+	() => {
+		validateMap('testProp', { a: Number }, options, { a: 1 });
+		validateMap('testProp', { a: String }, options, { a: '1' });
+		validateMap('testProp', { a: Boolean }, options, { a: true });
+		validateMap('testProp', { a: Array }, options, { a: [ 1, 2 ] });
+		validateMap('testProp', { a: Object }, options, { a: { c: 1 } });
+		validateMap('testProp', { a: Symbol }, options, { a: Symbol.iterator });
+
+		validateMap('testProp', { a: { type: Number } }, options, { a: 1 });
+		validateMap('testProp', { a: { type: String } }, options, { a: '1' });
+		validateMap('testProp', { a: { type: Boolean } }, options, { a: true });
+		validateMap('testProp', { a: { type: Array } }, options, { a: [ 1, 2 ] });
+		validateMap('testProp', { a: { type: Object } }, options, { a: { c: 1 } });
+		validateMap('testProp', { a: { type: Symbol } }, options, { a: Symbol.iterator });
+
+		expect(validateValue).toBeCalledTimes(12);
+		expect(validateObject).not.toBeCalled();
+
+		// @ts-ignore
+		validateValue.mockReset();
+	}
+);
+
+test('validateMap should invoke validateObject and itself recursively if validator is an object', () => {
+	const mock = jest.spyOn(validateMapExport, 'default');
+	validateMap('testProp', { a: {} }, options, { a: {} });
+	expect(validateObject).toBeCalled();
+	expect(mock).toBeCalledTimes(2);
+});
+
+test('validateMap should throw an error if property on the blueprint is not of a valid type.', () => {
+	const validateFn = () => validateMap('testProp', { a: 1 }, options, { a: 1 });
+	expect(validateFn).toThrowError(
+		`Unsuported schema type for the property 'testProp.a' (number).` +
+			` Check the documentation for the supported schema types.`
+	);
+});
+
+test('validateObject should invoke validate function (if provided) with the property name and the provided value.', () => {
+	const validateTest = jest.fn();
+
+	validateMap('testProp', { a: Number }, { validate: validateTest }, { a: 1 });
+	expect(validateTest).toBeCalledWith('testProp', { a: 1 });
+
+	validateMap('testProp2', { a: Number }, { validate: validateTest }, { a: 2 });
+	expect(validateTest).toBeCalledWith('testProp2', { a: 2 });
+});
+
+test('validateMap should throw an error if provided validate property is not a function.', () => {
+	const val1: any = 'string';
+	const fnStr = () => validateMap('testProp', { a: Number }, { validate: val1 }, { a: 1 });
+	expect(fnStr).toThrowError('Validator for the property testProp should be a function, string found instead.');
+
+	const val2: any = 1;
+	const fnNum = () => validateMap('testProp', { a: Number }, { validate: val2 }, { a: 1 });
+	expect(fnNum).toThrowError('Validator for the property testProp should be a function, number found instead.');
+
+	const val3: any = true;
+	const fnBool = () => validateMap('testProp', { a: Number }, { validate: val3 }, { a: 1 });
+	expect(fnBool).toThrowError('Validator for the property testProp should be a function, boolean found instead.');
+
+	const val4: any = Symbol.iterator;
+	const fnSym = () => validateMap('testProp', { a: Number }, { validate: val4 }, { a: 1 });
+	expect(fnSym).toThrowError('Validator for the property testProp should be a function, symbol found instead.');
+
+	const val5: any = [];
+	const fnArr = () => validateMap('testProp', { a: Number }, { validate: val5 }, { a: 1 });
+	expect(fnArr).toThrowError('Validator for the property testProp should be a function, array found instead.');
+
+	const val6: any = {};
+	const fnObj = () => validateMap('testProp', { a: Number }, { validate: val6 }, { a: 1 });
+	expect(fnObj).toThrowError('Validator for the property testProp should be a function, object found instead.');
+});
