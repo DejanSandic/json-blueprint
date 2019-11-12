e24b104270e67a88d11e3c07ca4c1a64e99302c6
Dejan Sandic <dejan.s@aleacontrol.com>
2019-11-12 14:46:16 +0100

    Add create and validate functions to the blueprint class

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,36 @@
+import validateObject from './validators/validateObject';
+import validateMap from './validators/validateMap';
+
 interface KeyValue {
-   [key: string]: any;
+	[key: string]: any;
 }
 
 class Blueprint {
-   _schema: KeyValue;
+	_blueprint: KeyValue;
 
-   constructor (schema: KeyValue) {
-      this._schema = schema;
-   }
+	constructor(blueprint: KeyValue) {
+		if (typeof blueprint === 'undefined') {
+			throw new Error('Type of blueprint is expected to be an object, undefined found instead.');
+		}
 
-   create (schema: KeyValue) {
-      return new Blueprint(schema);
-   }
+		validateObject('blueprint', {}, blueprint);
+		this._blueprint = blueprint;
+	}
 
-   validate (data: KeyValue) {}
+	static create(blueprint: KeyValue) {
+		return new Blueprint(blueprint);
+	}
+
+	validate(data: KeyValue) {
+		if (typeof data === 'undefined') {
+			throw new Error(
+				'Type of data provided to the Blueprint.validate() function ' +
+					'is expected to be an object, undefined found instead.'
+			);
+		}
+		validateObject('data provided to the Blueprint.validate() function', {}, data);
+		validateMap(null, this._blueprint, {}, data);
+	}
 }
 
 export default Blueprint;
