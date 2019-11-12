61d11132209a3d33e3cb8812ed66f0bca0d82962
Dejan Sandic <dejan.s@aleacontrol.com>
2019-11-12 14:47:29 +0100

    Fix linting issues for the Blueprint class and tests

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,35 @@ import validateObject from './validators/validateObject';
 import validateMap from './validators/validateMap';
 
 interface KeyValue {
-	[key: string]: any;
+   [key: string]: any;
 }
 
 class Blueprint {
-	_blueprint: KeyValue;
+   _blueprint: KeyValue;
 
-	constructor(blueprint: KeyValue) {
-		if (typeof blueprint === 'undefined') {
-			throw new Error('Type of blueprint is expected to be an object, undefined found instead.');
-		}
+   constructor (blueprint: KeyValue) {
+      if (typeof blueprint === 'undefined') {
+         throw new Error('Type of blueprint is expected to be an object, undefined found instead.');
+      }
 
-		validateObject('blueprint', {}, blueprint);
-		this._blueprint = blueprint;
-	}
+      validateObject('blueprint', {}, blueprint);
+      this._blueprint = blueprint;
+   }
 
-	static create(blueprint: KeyValue) {
-		return new Blueprint(blueprint);
-	}
+   static create (blueprint: KeyValue) {
+      return new Blueprint(blueprint);
+   }
 
-	validate(data: KeyValue) {
-		if (typeof data === 'undefined') {
-			throw new Error(
-				'Type of data provided to the Blueprint.validate() function ' +
-					'is expected to be an object, undefined found instead.'
-			);
-		}
-		validateObject('data provided to the Blueprint.validate() function', {}, data);
-		validateMap(null, this._blueprint, {}, data);
-	}
+   validate (data: KeyValue) {
+      if (typeof data === 'undefined') {
+         throw new Error(
+            'Type of data provided to the Blueprint.validate() function ' +
+               'is expected to be an object, undefined found instead.'
+         );
+      }
+      validateObject('data provided to the Blueprint.validate() function', {}, data);
+      validateMap(null, this._blueprint, {}, data);
+   }
 }
 
 export default Blueprint;
--- test/index.test.ts
+++ test/index.test.ts
@@ -5,53 +5,53 @@ import * as validateObjectExport from '../src/validators/validateObject';
 import * as validateMapExport from '../src/validators/validateMap';
 
 const nameAndAgeBlueprint = {
-	name: String,
-	age: Number
+   name: String,
+   age: Number
 };
 
 const nameAndAge = {
-	name: 'Blue Moon',
-	age: 1
+   name: 'Blue Moon',
+   age: 1
 };
 
 test(
-	'Calling new Blueprint and Blueprint.create should invoke validateObject' +
-		' function and create new blueprint instance.',
-	() => {
-		const validateObject = jest.spyOn(validateObjectExport, 'default');
-		const blueprint = new Blueprint(nameAndAgeBlueprint);
-		expect(blueprint._blueprint).toBe(nameAndAgeBlueprint);
+   'Calling new Blueprint and Blueprint.create should invoke validateObject' +
+      ' function and create new blueprint instance.',
+   () => {
+      const validateObject = jest.spyOn(validateObjectExport, 'default');
+      const blueprint = new Blueprint(nameAndAgeBlueprint);
+      expect(blueprint._blueprint).toBe(nameAndAgeBlueprint);
 
-		const blueprint2 = Blueprint.create(nameAndAgeBlueprint);
-		expect(blueprint2._blueprint).toBe(nameAndAgeBlueprint);
+      const blueprint2 = Blueprint.create(nameAndAgeBlueprint);
+      expect(blueprint2._blueprint).toBe(nameAndAgeBlueprint);
 
-		expect(validateObject).toBeCalledTimes(2);
-	}
+      expect(validateObject).toBeCalledTimes(2);
+   }
 );
 
 test('Calling new Blueprint should throw an error if provided blueprint is undefined.', () => {
-	const val: any = undefined;
-	const fn = () => new Blueprint(val);
-	expect(fn).toThrowError('Type of blueprint is expected to be an object, undefined found instead.');
+   const val: any = undefined;
+   const fn = () => new Blueprint(val);
+   expect(fn).toThrowError('Type of blueprint is expected to be an object, undefined found instead.');
 });
 
 test('Calling validate() method should throw an error if provided data is undefined.', () => {
-	const blueprint = new Blueprint(nameAndAgeBlueprint);
-	const val: any = undefined;
-	const fn = () => blueprint.validate(val);
-	expect(fn).toThrowError(
-		'Type of data provided to the Blueprint.validate()' +
-			' function is expected to be an object, undefined found instead.'
-	);
+   const blueprint = new Blueprint(nameAndAgeBlueprint);
+   const val: any = undefined;
+   const fn = () => blueprint.validate(val);
+   expect(fn).toThrowError(
+      'Type of data provided to the Blueprint.validate()' +
+         ' function is expected to be an object, undefined found instead.'
+   );
 });
 
 test('Calling validate() method should invoke validateObject and validateMap functions if provided data is an object.', () => {
-	const validateObject = jest.spyOn(validateObjectExport, 'default');
-	const validateMap = jest.spyOn(validateMapExport, 'default');
+   const validateObject = jest.spyOn(validateObjectExport, 'default');
+   const validateMap = jest.spyOn(validateMapExport, 'default');
 
-	const blueprint = new Blueprint(nameAndAgeBlueprint);
-	blueprint.validate(nameAndAge);
+   const blueprint = new Blueprint(nameAndAgeBlueprint);
+   blueprint.validate(nameAndAge);
 
-	expect(validateObject).toBeCalledWith('data provided to the Blueprint.validate() function', {}, nameAndAge);
-	expect(validateMap).toBeCalledWith(null, blueprint._blueprint, {}, nameAndAge);
+   expect(validateObject).toBeCalledWith('data provided to the Blueprint.validate() function', {}, nameAndAge);
+   expect(validateMap).toBeCalledWith(null, blueprint._blueprint, {}, nameAndAge);
 });
