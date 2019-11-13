4a66613e2f5352e94a70e310097d2f85bfa9fc67
Dejan Sandic <dejan.s@aleacontrol.com>
2019-11-13 13:09:03 +0100

    Add intergration test

---
+++ test/intergration.test.ts
@@ -0,0 +1,61 @@
+import Blueprint from '../src';
+
+test('Intergration test should not throw error.', () => {
+	const bp = new Blueprint({
+		name: String,
+		age: Number,
+		verified: {
+			type: Boolean,
+			required: false
+		},
+		phone: {
+			ext: String,
+			num: Number
+		},
+		a: {
+			b: {
+				c: String
+			}
+		},
+		d: Object,
+		tags: [
+			{
+				type: String
+			}
+		],
+		addresses: [
+			[
+				{
+					number: Number,
+					street: String
+				}
+			]
+		]
+	});
+
+	bp.validate({
+		name: 'Blue Moon',
+		age: 1,
+		phone: {
+			ext: '065',
+			num: 973020
+		},
+		a: {
+			b: {
+				c: '1'
+			}
+		},
+		d: {
+			c: 1
+		},
+		tags: [ '1', '2', '3' ],
+		addresses: [
+			[
+				{
+					number: 13,
+					street: 'NPM'
+				}
+			]
+		]
+	});
+});
